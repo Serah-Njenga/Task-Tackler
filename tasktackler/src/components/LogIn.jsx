f1d5a0f10453e3f0dbe8583176aa14fa0ef1177d
Serah-Njenga <serah35.nn@gmail.com>
2023-06-08 11:42:55 +0300

    commit

---
+++ tasktackler/src/components/LogIn.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+
+function LogIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useNavigate();
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:9292/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        history.push('/dashboard');
+      } else {
+        throw new Error('Invalid email or password');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} required />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} required />
+      </label>
+      <br />
+      <NavLink to="/dashboard"><button >Login</button></NavLink> 
+    </form>
+  );
+}
+
+export default LogIn;
+
+
+           
+      
